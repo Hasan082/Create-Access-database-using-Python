7b9f4c853fd55088c8d22bf49785a311d6a1bb36
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-10 15:17:25 +0600

    Create database and create table field

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import sqlite3
 
-conn = sqlite3.connect('INSTRUCTOR.db')
+# CREATE A DATABASE
+conn = sqlite3.connect('instructor.db')
 cursor_obj = conn.cursor()
 
+# DROP TABLE IF EXIST
+cursor_obj.execute("DROP TABLE IF EXISTS INSTRUCTOR")
+# CREATE TABLE SQL QUERY
+table = """
+CREATE TABLE IF NOT EXISTS INSTRUCTOR (
+    ID INTEGER PRIMARY KEY NOT NULL,
+    FNAME VARCHAR(30) NOT NULL,
+    LNAME VARCHAR(30) NOT NULL,
+    CITY VARCHAR(30),
+    CCODE CHAR(2)
+);
+"""
+# PASS QUERY TO EXECUTE
+cursor_obj.execute(table)
+conn.commit()
+conn.close()
