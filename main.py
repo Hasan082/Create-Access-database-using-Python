ffcc922d401e3b3e3a39535fbf58f38518af2ff8
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-10 16:18:48 +0600

    Data inserted successfully to db

--- main.py
+++ main.py
@@ -9,14 +9,22 @@ cursor_obj.execute("DROP TABLE IF EXISTS INSTRUCTOR")
 # CREATE TABLE SQL QUERY
 table = """
 CREATE TABLE IF NOT EXISTS INSTRUCTOR (
-    ID INTEGER PRIMARY KEY NOT NULL,
+    ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     FNAME VARCHAR(30) NOT NULL,
     LNAME VARCHAR(30) NOT NULL,
     CITY VARCHAR(30),
     CCODE CHAR(2)
 );
 """
-# PASS QUERY TO EXECUTE
+# Execute the SQL statement using the cursor object
 cursor_obj.execute(table)
+
+# push data to database
+insert_data = """INSERT INTO INSTRUCTOR (FNAME, LNAME, CITY, CCODE) VALUES ('Md', 'Hasan' , 'Pangsha', '10'), ('Mollika', 'Mukta' , 'Lakmoni', 
+'11'), ('Faria', 'Maahi' , 'Dhaka', '12');"""
+cursor_obj.execute(insert_data)
+
+# Commit the changes (important for data integrity)
 conn.commit()
+# Close the connection
 conn.close()
